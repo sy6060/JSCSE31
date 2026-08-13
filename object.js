532b32339af9fd15515a041724330384f4c5c3ea
Sanvi Yadav <sanvi.25b01010431@abes.ac.in>
2026-08-13 13:53:16 +0530

    Add files via upload

---
+++ object.js
@@ -0,0 +1,15 @@
+//array square brackets
+let marks=[1,2,3,4,5]
+
+//object key:value
+let student={
+    name:"sanvi yadav",
+    roll_no:2503201001006,
+    cgpa:9.3,
+    isPass:true, 
+};   //obj is terminated with semicolon
+//console.log(student);
+//console.log(student.name);
+student.name="saumya";
+//console.log(student);
+console.table(student);  //to output in a table format in terminal
