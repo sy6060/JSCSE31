839f31a91dfd7a7a2db0ca79935fbb74fb48b3bf
sanvi yadav <sanvi.25b01010431.abes.ac.in>
2026-08-17 10:48:59 +0530

    n

---
+++ ob2.js
@@ -0,0 +1,6 @@
+let obj={
+    name:"pen",
+    price:10
+}
+//console.log("the cost of",obj.name,"is",obj.price);
+console.log(`the cost of ${obj.name} is ${obj.price}`);
---
+++ table.js
@@ -0,0 +1,16 @@
+// object.js
+const students = [
+  { name: "Aarav", city: "Delhi", cgpa: 8.9 },
+  { name: "Ishita", city: "Mumbai", cgpa: 9.1 },
+  { name: "Rohan", city: "Bangalore", cgpa: 7.8 },
+  { name: "Meera", city: "Chennai", cgpa: 8.5 },
+  { name: "Kabir", city: "Hyderabad", cgpa: 9.3 },
+  { name: "Ananya", city: "Kolkata", cgpa: 8.7 },
+  { name: "Dev", city: "Pune", cgpa: 7.9 },
+  { name: "Sanya", city: "Jaipur", cgpa: 9.0 },
+  { name: "Arjun", city: "Lucknow", cgpa: 8.6 },
+  { name: "Priya", city: "Noida", cgpa: 9.2 }
+];
+
+// Display as table in terminal
+console.table(students);
