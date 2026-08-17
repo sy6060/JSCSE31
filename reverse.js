e6cfbbe4742239e14bad768b7cd7c4887bdded83
Sanvi Yadav <sanvi.25b01010431@abes.ac.in>
2026-08-17 11:18:08 +0530

    Add files via upload

---
+++ reverse.js
@@ -0,0 +1,7 @@
+//reverse a string
+let str = "Hello, World!";
+let reversedStr = "";
+for(let i=str.length - 1; i >= 0; i--) {
+  reversedStr += str[i];
+}
+console.log("Reversed string: " + reversedStr);
