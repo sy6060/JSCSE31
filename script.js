e5865264329d94c682ca46ea41973dbc2f07cff5
Sanvi Yadav <sanvi.25b01010431@abes.ac.in>
2026-08-17 12:12:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function turnOnBulb() {
+    document.getElementById('img1').style.display = 'none';
+    document.getElementById('img2').style.display = 'block';
+}
+
+function turnOffBulb() {
+    document.getElementById('img2').style.display = 'none';
+    document.getElementById('img1').style.display = 'block';
+}
